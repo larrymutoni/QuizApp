498697dbf32bbf4b23b0ab0ccf56c918c422b630
larrymutoni <larrycarnot@gamil.com>
2024-03-31 13:16:00 +0300

    solved #3

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 </script>
 
 <template>
-  <div class="conatiner">
+  <div class="container">
     <RouterView />
   </div>
 </template>
---
+++ src/views/QuizView.vue
@@ -0,0 +1,42 @@
+
+<script setup>
+    import Question from "../components/Question.vue"
+    import QuizHeader from "../components/QuizHeader.vue"
+
+    import {useRoute} from "vue-router"
+    import { ref, watch, computed } from "vue"
+    import quizes from "../data/quizes.json"
+
+    const route = useRoute()
+
+    const quizId = parseInt(route.params.id)
+
+    const quiz = quizes.find(q => q.id === quizId)
+
+    const currentQuestionIndex = ref(0)
+
+    // const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`)
+
+    // watch(() => currentQuestionIndex.value, () => {
+    //     questionStatus.value =  `${currentQuestionIndex.value}/${quiz.questions.length}`
+    // })
+
+    const questionStatus = computed(() => `${currentQuestionIndex.value}/${quiz.questions.length}`)
+
+    const barPercentage = computed(() => `${currentQuestionIndex.value/quiz.questions.length * 100}%`)
+</script>
+
+<template>
+    <div>
+        <QuizHeader 
+            :questionStatus="questionStatus" 
+            :barPercentage="barPercentage"
+        />
+        <div>
+            <Question :question="quiz.questions[currentQuestionIndex ]" />
+        </div>
+
+        <button @click="currentQuestionIndex++">Next Question</button>
+
+    </div>
+</template>
